f276f63061cd14e0da14120ae3aca36a0f8f1ad2
Erfan <erfantkh20073@gmail.com>
2025-05-27 18:48:07 +0330

    fix some buggs

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/Blog-mini-project",
+  assetPrefix: "/Blog-mini-project/",
+};
+
+module.exports = nextConfig;
